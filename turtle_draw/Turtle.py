f831390a3f28b435c5f2ae4135ec348647a34940
Jundel Malazarte <jundelmalazarte348@gmail.com>
2025-12-16 19:50:39 +0800

    Added ./turtle_draw/ Drawing using turtle library.

---
+++ turtle_draw/Turtle.py
@@ -0,0 +1,34 @@
+# Testing Turtle patterns/drawing 
+
+import turtle
+from turtle import *
+
+# Drawing a Triangle
+forward(100)
+left(120)
+forward(100)
+left(120)
+forward(100)
+
+# Drawing a Square
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+
+#drawwing a pentagon
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
